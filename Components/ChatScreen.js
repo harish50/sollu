27cceea349ff608ccdc6950193697e25611cac5b
vgaddam <vgaddam@aconex.com>
2019-02-06 22:48:51 +0530

    Filter messages day by day

--- Components/ChatScreen.js
+++ Components/ChatScreen.js
@@ -48,7 +48,41 @@ export default class ChatScreen extends React.Component {
             }
         );
     };
-    renderItem({ item }) {
+  
+    sendMessage() {
+        if (this.state.typing.trim() === '') {
+            return;
+        }
+        let { navigation } = this.props;
+        let db = firebase.database();
+        const info = navigation.getParam('info');
+        let msg = {
+            _id: 2,
+            text: this.state.typing.trim(),
+            createdAt: new Date().getTime(),
+        };
+        if (info.sender === info.receiver.item.key) {
+            msg._id = 0;
+            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver.item.key).push(msg);
+        }
+        else {
+            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver.item.key).push(msg);
+            msg._id = 1;
+            db.ref('registeredUsers').child(info.receiver.item.key).child("chat").child(info.sender).push(msg);
+        }
+        this.setState({ typing: '' });
+    }
+
+    // renderDayMessagesMap = (dayMessages) => {
+    //     let result = dayMessages.map((message) => {
+    //         return this.renderItem(message)
+    //     });
+
+
+    //     return (result)
+    // }
+    renderItem(item) {
+        console.log(item);
         let messageboxstyle;
         let messagetextstyle;
         if (item._id === 0) {
@@ -74,44 +108,46 @@ export default class ChatScreen extends React.Component {
             </View>
         );
     };
-    sendMessage() {
-        if (this.state.typing.trim() === '') {
-            return;
-        }
-        let { navigation } = this.props;
-        let db = firebase.database();
-        const info = navigation.getParam('info');
-        let msg = {
-            _id: 2,
-            text: this.state.typing.trim(),
-            createdAt: new Date().getTime(),
-        };
-        if (info.sender === info.receiver.item.key) {
-            msg._id = 0;
-            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver.item.key).push(msg);
-        }
-        else {
-            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver.item.key).push(msg);
-            msg._id = 1;
-            db.ref('registeredUsers').child(info.receiver.item.key).child("chat").child(info.sender).push(msg);
-        }
-        this.setState({ typing: '' });
+
+    renderDayMessages = (dayMessages, day) => {
+       dayMessages.map((message)=> {
+           return this.renderItem(message);
+       })
     }
+
+    renderMessages = (messages) => {
+        let preMsgDate;
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        if (messages[0]) {
+            preMsgDate = monthNames[messages[0].createdAt.getMonth()] + " " + messages[0].createdAt.getDate() + " " + messages[0].createdAt.getFullYear();
+        }
+        let dayMessages = [];
+         messages.map((message) => {
+            let fullDate = monthNames[message.createdAt.getMonth()] + " " + message.createdAt.getDate() + " " + message.createdAt.getFullYear();
+            if (fullDate === preMsgDate) {
+                dayMessages.push(message);
+            }
+            else {
+                let result = this.renderDayMessages(dayMessages, preMsgDate);
+                dayMessages = [];
+                preMsgDate = fullDate;
+                dayMessages.push(message);
+                return result;
+            }
+        });
+        return this.renderDayMessages(dayMessages,preMsgDate);
+    }
+
     render() {
         const keyboardVerticalOffset = Platform.OS === 'ios' ? Header.HEIGHT + 20 : 0;
         const padding = Platform.OS === 'ios' ? "padding" : '';
         return (
             <SafeAreaView style={styles.safeAreaView}>
                 <View style={styles.container}>
-                    <FlatList
-                        data={this.state.messages}
-                        renderItem={this.renderItem.bind(this)}
-                        extraData={this.state}
-                        keyExtractor={(item, index) => index.toString()}
-                        ref={ref => this.flatList = ref}
-                        onContentSizeChange={() => this.flatList.scrollToEnd({ animated: false })}
-                        onLayout={() => this.flatList.scrollToEnd({ animated: true })}
-                    />
+                    <View style={styles.container}>
+                        {this.renderMessages(this.state.messages)}
+                    </View>
+
                     <KeyboardAvoidingView
                         keyboardVerticalOffset={keyboardVerticalOffset}
                         behavior={padding}
