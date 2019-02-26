116aa2941b515865e57b4230371d8eed537a444f
GAnjali <anjali.gaddam98@gmail.com>
2019-02-27 00:23:51 +0530

    establish a peer to peer connection

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,15 +1,34 @@
 import React from "react";
-import { View, Text, FlatList, TouchableOpacity, Platform, PermissionsAndroid, ActivityIndicator, StyleSheet, AsyncStorage } from 'react-native';
+import {
+    View,
+    Text,
+    FlatList,
+    TouchableOpacity,
+    Platform,
+    PermissionsAndroid,
+    ActivityIndicator,
+    StyleSheet,
+    AsyncStorage
+} from "react-native";
 import styles from "../Stylesheet/styleSheet";
-import Contacts from 'react-native-contacts';
-import firebase from '../firebase/firebase';
-import Profile from './Profile';
-import Firebase from 'react-native-firebase';
+import Contacts from "react-native-contacts";
+import firebase from "../firebase/firebase";
+import Profile from "./Profile";
+import Firebase from "react-native-firebase";
+import {
+    RTCPeerConnection,
+    RTCIceCandidate,
+    RTCSessionDescription,
+    RTCView,
+    MediaStream,
+    MediaStreamTrack,
+    mediaDevices
+} from "react-native-webrtc";
 
 export default class HomeScreen extends React.Component {
     state = {
         contacts: [],
-        currentUser: '',
+        currentUser: ""
     };
 
     async checkPermission() {
@@ -22,25 +41,33 @@ export default class HomeScreen extends React.Component {
     }
 
     async getToken() {
-        let fcmToken = await AsyncStorage.getItem('fcmToken');
+        let fcmToken = await AsyncStorage.getItem("fcmToken");
         if (!fcmToken) {
             fcmToken = await Firebase.messaging().getToken();
             if (fcmToken) {
-                await AsyncStorage.setItem('fcmToken', fcmToken);
+                await AsyncStorage.setItem("fcmToken", fcmToken);
             }
         }
         const user = this.props.navigation.getParam("sender");
-        firebase.database().ref('registeredUserProfileInfo').child(user).child("pushToken").set(fcmToken);
+        firebase
+            .database()
+            .ref("registeredUserProfileInfo")
+            .child(user)
+            .child("pushToken")
+            .set(fcmToken);
     }
+
+    //videocall receiving side
 
     async requestPermission() {
         try {
             await Firebase.messaging().requestPermission();
             this.getToken();
         } catch (error) {
-            console.log('permission rejected');
+            console.log("permission rejected");
         }
     }
+
     componentWillUnmount() {
         this.notificationListener();
         this.notificationOpenedListener();
@@ -48,51 +75,62 @@ export default class HomeScreen extends React.Component {
 
     async createNotificationListeners() {
         const channel = new Firebase.notifications.Android.Channel(
-            '001',
-            'Channel Name',
+            "001",
+            "Channel Name",
             Firebase.notifications.Android.Importance.Max
-        ).setDescription('A natural description of the channel');
+        ).setDescription("A natural description of the channel");
         Firebase.notifications().android.createChannel(channel);
 
-        this.notificationListener = Firebase.notifications().onNotification(async (notification) => {
+        this.notificationListener = Firebase.notifications().onNotification(
+            async notification => {
+                const contactName = await AsyncStorage.getItem(
+                    notification.data.sender
+                );
+                const data = {
+                    sender: notification.data.receiver,
+                    receiver: notification.data.sender
+                };
+                if (contactName === this.state.currentUser) {
+                    return;
+                }
+                const localNotification = new Firebase.notifications.Notification({
+                    show_in_foreground: true
+                })
+                    .setNotificationId(notification.notificationId)
+                    .setTitle(contactName)
+                    .setBody(notification.body)
+                    .setData(data)
+                    .setSound("default");
+                if (Platform.OS === "android") {
+                    localNotification.android
+                        .setAutoCancel(true)
+                        .android.setChannelId(channel.channelId)
+                        .android.setPriority(Firebase.notifications.Android.Priority.High);
+                } else if (Platform.OS === "ios") {
+                    localNotification.ios.setBadge(notification.ios.badge);
+                }
+                Firebase.notifications()
+                    .displayNotification(localNotification)
+                    .catch(err => console.error("cant send"));
+            }
+        );
 
-            const contactName = await AsyncStorage.getItem(notification.data.sender);
-            const data = {
-                sender: notification.data.receiver,
-                receiver: notification.data.sender,
+        this.notificationOpenedListener = Firebase.notifications().onNotificationOpened(
+            async notificationOpen => {
+                const notification = notificationOpen.notification;
+                const data = notification.data;
+                const contactName = await AsyncStorage.getItem(data.receiver);
+                let info = {
+                    receiver: data.receiver,
+                    sender: data.sender
+                };
+                this.props.navigation.navigate(
+                    "ChatScreen",
+                    {info: info, contactName: contactName},
+                    {onGoBack: () => this.updateCurrentUser()}
+                );
             }
-            if (contactName === this.state.currentUser) {
-                return;
-            }
-            const localNotification = new Firebase.notifications.Notification({
-                show_in_foreground: true,
-            }).setNotificationId(notification.notificationId)
-                .setTitle(contactName)
-                .setBody(notification.body)
-                .setData(data)
-                .setSound('default');
-            if (Platform.OS === 'android') {
-                localNotification.android.setAutoCancel(true)
-                    .android.setChannelId(channel.channelId)
-                    .android.setPriority(Firebase.notifications.Android.Priority.High);
-            }
-            else if (Platform.OS === 'ios') {
-                localNotification.ios.setBadge(notification.ios.badge);
-            }
-            Firebase.notifications().displayNotification(localNotification)
-                .catch(err => console.error("cant send"));
-        });
-
-        this.notificationOpenedListener = Firebase.notifications().onNotificationOpened(async (notificationOpen) => {
-            const notification = notificationOpen.notification;
-            const data = notification.data;
-            const contactName = await AsyncStorage.getItem(data.receiver);
-            let info = {
-                receiver: data.receiver,
-                sender: data.sender,
-            }
-            this.props.navigation.navigate('ChatScreen', { info: info, contactName: contactName }, { onGoBack: () => this.updateCurrentUser() });
-        });
+        );
 
         const notificationOpen = await Firebase.notifications().getInitialNotification();
         if (notificationOpen) {
@@ -101,20 +139,24 @@ export default class HomeScreen extends React.Component {
             const contactName = await AsyncStorage.getItem(data.sender);
             let info = {
                 receiver: data.sender,
-                sender: data.receiver,
-            }
-            this.props.navigation.navigate('ChatScreen', { info: info, contactName: contactName }, { onGoBack: () => this.updateCurrentUser() });
+                sender: data.receiver
+            };
+            this.props.navigation.navigate(
+                "ChatScreen",
+                {info: info, contactName: contactName},
+                {onGoBack: () => this.updateCurrentUser()}
+            );
         }
-
     }
+
     updateCurrentUser() {
-        this.setState({ currentUser: '' });
-        this.setState({ contacts: [] });
+        this.setState({currentUser: ""});
+        this.setState({contacts: []});
         this.getOrderedLocalContacts();
     }
 
-    getPairID(sender,receiver){
-        let key='';
+    getPairID(sender, receiver) {
+        let key = "";
         if (sender === receiver) {
             key = sender;
         } else if (sender > receiver) {
@@ -125,20 +167,23 @@ export default class HomeScreen extends React.Component {
         return key;
     }
 
-     async getLastActiveTime(sender, receiver) {
-         let  lastchatTime=0;
-         let key = this.getPairID(sender, receiver);
-         await  firebase.database().ref('conversations').once('value',async (snap) => {
-             if (snap.hasChild(key)) {
-                 let time = snap.child(key).val().lastActiveTime;
-                 if (time){
-                     lastchatTime=time;
-                     return time;
-                 }
-             }
-         });
-         return lastchatTime;
-     }
+    async getLastActiveTime(sender, receiver) {
+        let lastchatTime = 0;
+        let key = this.getPairID(sender, receiver);
+        await firebase
+            .database()
+            .ref("conversations")
+            .once("value", async snap => {
+                if (snap.hasChild(key)) {
+                    let time = snap.child(key).val().lastActiveTime;
+                    if (time) {
+                        lastchatTime = time;
+                        return time;
+                    }
+                }
+            });
+        return lastchatTime;
+    }
 
     async requestContactsPermission() {
         try {
@@ -150,15 +195,16 @@ export default class HomeScreen extends React.Component {
             return Platform.OS === "ios" ? true : false;
         }
     }
-    getOrderedLocalContacts(){
+
+    getOrderedLocalContacts() {
         let db = firebase.database();
         let localContacts = [];
-        let sender=this.props.navigation.getParam("sender");
+        let sender = this.props.navigation.getParam("sender");
         Contacts.getAll((err, contacts) => {
             if (err) throw err;
             else {
-                db.ref("registeredUsers").once('value', async (registeredUsers) => {
-                    const time= await this.getLastActiveTime(sender,sender);
+                db.ref("registeredUsers").once("value", async registeredUsers => {
+                    const time = await this.getLastActiveTime(sender, sender);
                     localContacts.push({
                         key: sender,
                         name: "You",
@@ -166,7 +212,10 @@ export default class HomeScreen extends React.Component {
                     });
                     for (let i = 0; i < contacts.length; i++) {
                         if (contacts[i].phoneNumbers.length !== 0) {
-                            let number = contacts[i].phoneNumbers[0].number.replace(/\D/g, '');
+                            let number = contacts[i].phoneNumbers[0].number.replace(
+                                /\D/g,
+                                ""
+                            );
                             let trimmedNumber = number;
                             if (number.length == 12) {
                                 trimmedNumber = number.substring(2);
@@ -174,7 +223,7 @@ export default class HomeScreen extends React.Component {
                             number = trimmedNumber;
                             if (number) {
                                 if (number && registeredUsers.hasChild(number)) {
-                                    const time = await this.getLastActiveTime(sender,number);
+                                    const time = await this.getLastActiveTime(sender, number);
                                     localContacts.push({
                                         key: number,
                                         name: contacts[i].givenName,
@@ -185,14 +234,49 @@ export default class HomeScreen extends React.Component {
                             }
                         }
                     }
-                    localContacts.sort(function(contact1, contact2){return contact2.lastActiveTime - contact1.lastActiveTime});
+                    localContacts.sort(function (contact1, contact2) {
+                        return contact2.lastActiveTime - contact1.lastActiveTime;
+                    });
                     this.setState({
                         contacts: [...this.state.contacts, ...localContacts]
-                    })
+                    });
                 });
             }
-        })
+        });
     }
+
+    listenForVideoCall() {
+        let VIDEO_CALL_REF = firebase.database().ref("videoCallInfo");
+        if (VIDEO_CALL_REF.hasChild(this.props.navigation.getParam("sender"))) {
+            VIDEO_CALL_REF.child(this.props.navigation.getParam("sender") + "/isVideoReceiveCall").on("once", videoInfo => {
+                if (videoInfo.val() === true) {
+                    const pc = new RTCPeerConnection();
+                    navigator.mediaDevices
+                        .getUserMedia({
+                            audio: true,
+                            video: true
+                        })
+                        .then(function (stream) {
+                            pc.addStream(stream);
+                            pc.createAnswer()
+                                .then(sdp => pc.setLocalDescription(sdp)
+                                );
+                            if (VIDEO_CALL_REF.hasChild(this.props.navigation.getParam("sender")))
+                                VIDEO_CALL_REF.child(this.props.navigation.getParam("sender")).child('videoSDP').push(pc.localDescription);
+                            VIDEO_CALL_REF.child(this.props.navigation.getParam("sender") + "/caller").on("once", caller => {
+                                let callerNum = caller.val();
+                                return VIDEO_CALL_REF.child(callerNum).once("once", callerSDP => {
+                                    pc.setRemoteDescription(callerSDP.val());
+                                    console.log(callerSDP.val());
+                                });
+                            })
+                            ;
+                        });
+                }
+            });
+        }
+    }
+
     async componentDidMount() {
         const permission = await this.requestContactsPermission();
         if (!permission) {
@@ -202,56 +286,64 @@ export default class HomeScreen extends React.Component {
         this.getOrderedLocalContacts();
         this.checkPermission();
         this.createNotificationListeners();
+        this.listenForVideoCall();
     }
 
     renderName(contact) {
         let info = {
             sender: this.props.navigation.getParam("sender"),
             receiver: contact.item.key
-        }
+        };
         return (
-            <TouchableOpacity onPress={() => {
-                this.props.navigation.navigate('ChatScreen', { info: info, contactName: contact.item.name ,  onGoBack: () => this.updateCurrentUser()});
-                this.setState({ currentUser: contact.item.name })
-            }} style={styles.contactContainer}>
-                <Profile sender={contact.item.key} />
+            <TouchableOpacity
+                onPress={() => {
+                    this.props.navigation.navigate("ChatScreen", {
+                        info: info,
+                        contactName: contact.item.name,
+                        onGoBack: () => this.updateCurrentUser()
+                    });
+                    this.setState({currentUser: contact.item.name});
+                }}
+                style={styles.contactContainer}
+            >
+                <Profile sender={contact.item.key}/>
                 <Text style={styles.item}> {contact.item.name} </Text>
             </TouchableOpacity>
         );
     }
 
-    static navigationOptions = ({ navigation }) => {
+    static navigationOptions = ({navigation}) => {
         let props = navigation;
-        return (
-            {
-                headerTitle: 'Sollu',
-                headerBackTitle: "Back",
-                headerTintColor: "white",
-                headerStyle: {
-                    fontFamily: 'Roboto-Bold',
-                    backgroundColor: '#cc504e',
-                    height: 60,
-                },
-                headerRight: (<Profile sender={props.getParam("sender")} navigation={props} />),
-            }
-        );
+        return {
+            headerTitle: "Sollu",
+            headerBackTitle: "Back",
+            headerTintColor: "white",
+            headerStyle: {
+                fontFamily: "Roboto-Bold",
+                backgroundColor: "#cc504e",
+                height: 60
+            },
+            headerRight: (
+                <Profile sender={props.getParam("sender")} navigation={props}/>
+            )
+        };
     };
+
     render() {
         if (this.state.contacts.length === 0) {
-            return (<View style={styles.loadingIcon}>
-                <ActivityIndicator size="large" color='#cc504e' />
+            return (
+                <View style={styles.loadingIcon}>
+                    <ActivityIndicator size="large" color="#cc504e"/>
                     <View>
-                        <Text style={styles.loadingtextbox}>
-                            Loading Contacts...
-                        </Text>
+                        <Text style={styles.loadingtextbox}>Loading Contacts...</Text>
                     </View>
-            </View>
+                </View>
             );
         } else if (this.state.contacts.length <= 0) {
-            return (<View style={[styles.loadingIcon, styles.loadShape]}>
-                <Text style={styles.loadingText}>No Registered Contacts</Text>
-
-            </View>
+            return (
+                <View style={[styles.loadingIcon, styles.loadShape]}>
+                    <Text style={styles.loadingText}>No Registered Contacts</Text>
+                </View>
             );
         } else {
             return (
@@ -266,4 +358,3 @@ export default class HomeScreen extends React.Component {
         }
     }
 }
-
--- Components/VideoCall.js
+++ Components/VideoCall.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import styles from '../Stylesheet/videocallStyles'
-import FontAwesome, { Icons } from 'react-native-fontawesome';
-
+import React, {Component} from "react";
+import {View, Text, TouchableOpacity} from "react-native";
+import styles from "../Stylesheet/videocallStyles";
+import FontAwesome, {Icons} from "react-native-fontawesome";
 
 import {
     RTCPeerConnection,
@@ -12,111 +11,133 @@ import {
     MediaStream,
     MediaStreamTrack,
     mediaDevices
-} from 'react-native-webrtc'
+} from "react-native-webrtc";
+import firebase from "../firebase/firebase";
 
 export default class VideoCall extends Component {
     state = {
-        videoURL: null,
+        SenderVideoURL: null,
+        RecieverVideoURL: null,
         isFront: true
-    }
+    };
 
-    static navigationOptions = ({ navigation }) => {
-        let props = navigation
-        return ({
-            headerTitle: navigation.getParam('contactName'),
+    static navigationOptions = ({navigation}) => {
+        let props = navigation;
+        return {
+            headerTitle: navigation.getParam("contactName"),
             headerTintColor: "#cc504e",
             headerBackTitle: "Back",
             headerStyle: {
-                fontFamily: 'Roboto-Bold',
-                height: 60,
-            },
-        })
+                fontFamily: "Roboto-Bold",
+                height: 60
+            }
+        };
+    };
+
+    getPairID(sender, receiver) {
+        let key = "";
+        if (sender === receiver) {
+            key = sender;
+        } else if (sender > receiver) {
+            key = receiver + sender;
+        } else {
+            key = sender + receiver;
+        }
+        return key;
     }
 
     componentDidMount() {
-        const configuration = { "iceServers": [{ "url": "stun:stun.l.google.com:19302" }] };
+        const configuration = {
+            iceServers: [{url: "stun:stun.l.google.com:19302"}]
+        };
         const pc = new RTCPeerConnection(configuration);
-        const { isFront } = this.state;
+        const {isFront} = this.state;
         mediaDevices.enumerateDevices().then(sourceInfos => {
             console.log(sourceInfos);
             let videoSourceId;
             for (let i = 0; i < sourceInfos.length; i++) {
                 const sourceInfo = sourceInfos[i];
-                if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+                if (
+                    sourceInfo.kind == "video" &&
+                    sourceInfo.facing == (isFront ? "front" : "back")
+                ) {
                     videoSourceId = sourceInfo.id;
                 }
             }
 
-            mediaDevices.getUserMedia({
-                audio: true,
-                video: {
-                    mandatory: {
-                        minWidth: 320, // Provide your own width, height and frame rate here
-                        minHeight: 240,
-                        minFrameRate: 30
-                    },
-                    facingMode: (isFront ? "user" : "environment"),
-                    optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
-                }
-            })
+            mediaDevices
+                .getUserMedia({
+                    audio: true,
+                    video: {
+                        mandatory: {
+                            minWidth: 320, // Provide your own width, height and frame rate here
+                            minHeight: 240,
+                            minFrameRate: 30
+                        },
+                        facingMode: isFront ? "user" : "environment",
+                        optional: videoSourceId ? [{sourceId: videoSourceId}] : []
+                    }
+                })
                 .then(stream => {
-                    console.log('Streaming OK', stream);
+                    console.log("Streaming OK", stream);
+                    video.srcObject = stream;
                     this.setState({
-                        videoURL: stream.toURL()
+                        SenderVideoURL: stream.toURL()
                     });
                     pc.addStream(stream);
                     // Got stream!
                 })
                 .catch(error => {
-                    console.log('Oops, we getting error', error.message);
+                    console.log("Oops, we getting error", error.message);
                     throw error;
                 });
         });
-        // pc.createOffer((desc) => {
-        //     pc.setLocalDescription(desc, () => {
-        //         console.log('pc.setLocalDescription');
-        //     }, (e) => { throw e; });
-        // }, (e) => { throw e; });
-        //
-        // pc.onicecandidate = (event) => {
-        //     console.log('onicecandidate', event);
-        // };
-
+        //caller side
+        let info = this.props.navigation.getParam("info");
+        console.log(info);
+        pc.createOffer()
+            .then(sdp => pc.setLocalDescription(sdp))
+        let VIDEO_CALL_REF = firebase.database().ref("videoCallInfo");
+        VIDEO_CALL_REF.child(info.sender).child('videoSDP').push(pc.localDescription),
+            VIDEO_CALL_REF.child(info.sender).set({isVideoSendCall: true}),
+            VIDEO_CALL_REF.child(info.receiver).set({isVideoReceiveCall: true}),
+            VIDEO_CALL_REF.child(info.receiver).set({caller: info.sender})
+        if (VIDEO_CALL_REF.hasChild(info.receiver)!=null) {
+            let remoteSDP = VIDEO_CALL_REF.child(info.receiver).child("videoSDP").once("once", (receiverVideoSDP) => receiverVideoSDP.val());
+            pc.setRemoteDescription(remoteSDP);
+        }
     }
 
     handlePressCall = () => {
-        let { navigation } = this.props;
-        this.props.navigation.navigate('ChatScreen')
-    }
-    render() {
+        let {navigation} = this.props;
+        this.props.navigation.navigate("ChatScreen");
+    };
 
-        if (this.state.videoURL) {
-            console.warn(this.state.videoURL);
+    render() {
+        if (this.state.ReceiverVideoURL) {
+            console.warn(this.state.ReceiverVideoURL);
             return (
                 <View style={styles.container}>
-
-                        <RTCView streamURL={this.state.videoURL}  style={styles.video1}/>
-                        <View style={styles.callIcon}>
-                            <TouchableOpacity onPress={this.handlePressCall}>
-                                <Text style={styles.phoneCallBox}>
-                                    <FontAwesome>{Icons.phoneSquare}</FontAwesome>
-                                </Text>
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Text style={styles.phoneCallBox}>
-                                    <FontAwesome>{Icons.videoSlash}</FontAwesome>
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
+                    <RTCView streamURL={this.state.videoURL} style={styles.video1}/>
+                    <View style={styles.callIcon}>
+                        <TouchableOpacity onPress={this.handlePressCall}>
+                            <Text style={styles.phoneCallBox}>
+                                <FontAwesome>{Icons.phoneSquare}</FontAwesome>
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <Text style={styles.phoneCallBox}>
+                                <FontAwesome>{Icons.videoSlash}</FontAwesome>
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             );
-        }
-        else {
+        } else {
             return (
                 <View>
                     <Text style={styles.text}>Not Working On Web RTC</Text>
                 </View>
-
             );
         }
 
