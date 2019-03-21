3124a2aa035b5590d184919d708b679a11c50341
Harika-Raj <r121662@rguktrkv.ac.in>
2019-03-21 07:51:21 +0530

    Add small changes in UI part

--- Components/VideoIconComponent.js
+++ Components/VideoIconComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {TouchableOpacity,Text} from 'react-native';
-import FontAwesome, {Icons} from 'react-native-fontawesome';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from '../Stylesheet/videocallStyles'
 
 
@@ -18,7 +18,7 @@ export default class VideoIconComponent extends Component{
             <TouchableOpacity onPress={this.handlePress}>
                 <Text style={styles.videoIcon}>
                     {  (info.sender !== info.receiver) &&
-                    <FontAwesome>{Icons.video}</FontAwesome>
+                    <Icon name="videocam" color="#fff" size={40}/>
                     }
                 </Text>
             </TouchableOpacity>
