a96a82877f5fc78ca6707df3343fd014a6f77252
GAnjali <anjali.gaddam98@gmail.com>
2019-04-06 18:16:06 +0530

    refactor homescreen code excluding styles

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -7,9 +7,7 @@ import {
     Platform,
     Text,
     TouchableOpacity,
-    View,
-    Linking,
-    StyleSheet
+    View
 } from "react-native";
 import styles from "../Stylesheet/styleSheet";
 import Contacts from "react-native-contacts";
@@ -19,14 +17,160 @@ import Firebase from "react-native-firebase";
 
 let sender = null;
 let caller = null;
-let VIDEO_CALL_REF = firebase.database().ref("videoCallInfo");
+let info = {
+    receiver: '',
+    sender: ''
+};
+
+//firebase references
+let VIDEO_CALL_INFO_REF = firebase.database().ref("videoCallInfo");
+let REGISTERED_USER_PROFILE_INFO_REF = firebase.database().ref("registeredUserProfileInfo");
+let REGISTERED_USERS_REF = firebase.database().ref("registeredUsers");
+
 export default class HomeScreen extends React.Component {
+
     state = {
         contacts: [],
         currentUser: "",
         remoteStream: '',
         streamVideo: false
     };
+
+    componentWillMount() {
+        sender = this.props.navigation.getParam("sender");
+        REGISTERED_USER_PROFILE_INFO_REF.child(sender).child("LocalContacts").child(sender).set("You");
+    }
+
+    async componentDidMount() {
+        //requesting permission for getting local contacts
+        const permission = await this.requestContactsPermission();
+        if (!permission) {
+            alert(permission);
+            return;
+        }
+        await this.updateList();
+        this.storeContactNamesInDB();
+        this.checkPermission();//fcm permissions
+        this.createNotificationListeners();
+        this.listenForVideoCall();
+    }
+
+    componentWillUnmount() {
+        this.notificationListener();
+        this.notificationOpenedListener();
+        VIDEO_CALL_INFO_REF.child(sender).child("flag").remove();
+    }
+
+    async requestContactsPermission() {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.READ_CONTACTS
+            );
+            return granted === PermissionsAndroid.RESULTS.GRANTED;
+        } catch (error) {
+            return Platform.OS === "ios" ? true : false;
+        }
+    }
+
+
+    async updateList() {
+        let response = await AsyncStorage.getItem('localContacts');
+        if (response !== null) {
+            //getting local contacts from local store
+            let localContacts = await JSON.parse(response) || [];
+            this.setState({
+                contacts: localContacts
+            });
+        }
+        this.getLocalContactsFromDB();
+    }
+
+    async getLocalContactsFromDB() {
+        let localContacts = [];
+        Contacts.getAll((err, contacts) => {
+            if (err) throw err;
+            else {
+                REGISTERED_USERS_REF.once("value", async registeredUsers => {
+                    const time = await this.getLastActiveTime(sender, sender);
+                    localContacts.push({
+                        key: sender,
+                        name: "You",
+                        lastActiveTime: time
+                    });
+                    for (let i = 0; i < contacts.length; i++) {
+                        if (contacts[i].phoneNumbers.length !== 0) {
+                            let number = contacts[i].phoneNumbers[0].number.replace(/\D/g, "");
+                            let trimmedNumber = number;
+                            if (number.length === 12) {
+                                trimmedNumber = number.substring(2);
+                            }
+                            number = trimmedNumber;
+                            if (number) {
+                                if (number && registeredUsers.hasChild(number)) {
+                                    const time = await this.getLastActiveTime(sender, number);
+                                    localContacts.push({
+                                        key: number,
+                                        name: contacts[i].givenName,
+                                        lastActiveTime: time
+                                    });
+                                    AsyncStorage.setItem(number, contacts[i].givenName);
+                                }
+                            }
+                        }
+                    }
+                    localContacts.sort(function (contact1, contact2) {
+                        return contact2.lastActiveTime - contact1.lastActiveTime;
+                    });
+                    this.setState({
+                        contacts: localContacts
+                    });
+                    await AsyncStorage.setItem('localContacts', JSON.stringify(localContacts))
+                });
+            }
+        });
+    }
+
+    //storing contacts with names
+    storeContactNamesInDB=()=>{
+        let dbLocalContRef = REGISTERED_USER_PROFILE_INFO_REF.child(sender).child("LocalContacts");
+        dbLocalContRef.once("value", (contactsFromDb) => {
+            let myLocalContacts = this.state.contacts;
+            for (let index = 0; index < this.state.contacts.length; index++) {
+                if (!contactsFromDb.hasChild(myLocalContacts[index].key)) {
+                    console.log("It has the child");
+                    dbLocalContRef.child(myLocalContacts[index].key).set(myLocalContacts[index].name);
+                }
+            }
+        })
+    };
+
+    //getting last active of this chat for sorting of chats
+    async getLastActiveTime(sender, receiver) {
+        let lastchatTime = 0;
+        let key = this.getPairID(sender, receiver);
+        await firebase.database().ref('conversations').once('value', async (snap) => {
+            if (snap.hasChild(key)) {
+                let time = snap.child(key).val().lastActiveTime;
+                if (time) {
+                    lastchatTime = time;
+                    return time;
+                }
+            }
+        });
+        return lastchatTime;
+    }
+
+    getPairID(sender, receiver) {
+        let key = '';
+        if (sender === receiver) {
+            key = sender;
+        } else if (sender > receiver) {
+            key = receiver + sender;
+        } else {
+            key = sender + receiver;
+        }
+        return key;
+    }
 
     async checkPermission() {
         const enabled = await Firebase.messaging().hasPermission();
@@ -45,15 +189,8 @@ export default class HomeScreen extends React.Component {
                 await AsyncStorage.setItem("fcmToken", fcmToken);
             }
         }
-        firebase
-            .database()
-            .ref("registeredUserProfileInfo")
-            .child(sender)
-            .child("pushToken")
-            .set(fcmToken);
+        REGISTERED_USER_PROFILE_INFO_REF.child(sender).child("pushToken").set(fcmToken);
     }
-
-    //videocall receiving side
 
     async requestPermission() {
         try {
@@ -62,12 +199,6 @@ export default class HomeScreen extends React.Component {
         } catch (error) {
             console.log("permission rejected");
         }
-    }
-
-    componentWillUnmount() {
-        this.notificationListener();
-        this.notificationOpenedListener();
-        VIDEO_CALL_REF.child(sender).child("flag").remove();
     }
 
     async createNotificationListeners() {
@@ -81,8 +212,7 @@ export default class HomeScreen extends React.Component {
         this.notificationListener = Firebase.notifications().onNotification(
             async notification => {
                 console.log("onNotofication")
-                if(notification.body === "Incoming video call. Tap to open")
-                {
+                if (notification.body === "Incoming video call. Tap to open") {
                     return;
                 }
                 const contactName = await AsyncStorage.getItem(
@@ -113,225 +243,74 @@ export default class HomeScreen extends React.Component {
                 }
                 Firebase.notifications()
                     .displayNotification(localNotification)
-                    .catch(err => console.error("cant send"));
+                    .catch(err => console.error("can't send"));
             }
-
         );
 
         this.notificationOpenedListener = Firebase.notifications().onNotificationOpened(
             async notificationOpen => {
-                    const notification = notificationOpen.notification
-                    const data = notification.data;
-                    const contactName = await AsyncStorage.getItem(data.receiver);
-                    let info = {
-                        receiver: data.receiver,
-                        sender: data.sender
-                    };
-                    this.props.navigation.navigate(
-                        "ChatScreen",
-                        {info: info, contactName: contactName},
-                        {onGoBack: () => this.updateCurrentUser()}
-                    );
+                this.navigateToChatScreen(notificationOpen.notification);
             }
         );
 
         const notificationOpen = await Firebase.notifications().getInitialNotification()
         if (notificationOpen) {
-            console.log("getInitialNotification");
-            if(notificationOpen.notification.data.receiver === undefined){
-                console.log("if passed")
+            if (notificationOpen.notification.data.receiver === undefined)
                 this.listenForVideoCall();
-            }
-            else{
-                const notification = notificationOpen.notification;
-                const data = notification.data;
-                const contactName = await AsyncStorage.getItem(data.sender);
-                let info = {
-                    receiver: data.sender,
-                    sender: data.receiver
-                };
-                this.props.navigation.navigate(
-                    "ChatScreen",
-                    {info: info, contactName: contactName},
-                    {onGoBack: () => this.updateCurrentUser()}
-                );
-            }
+            else
+                this.navigateToChatScreen(notificationOpen.notification);
         }
-
-
     }
+
+    navigateToChatScreen= async (notification) => {
+        let contactName = await AsyncStorage.getItem(data.sender);
+        info.sender = notification.data.sender;
+        info.receiver = notification.data.receiver;
+        this.props.navigation.navigate(
+            "ChatScreen",
+            {info: info, contactName: contactName},
+            {onGoBack: () => this.updateCurrentUser()}
+        );
+    };
+
     async updateCurrentUser() {
-        this.setState({ currentUser: '' });
-        this.setState({ contacts: []});
+        this.setState({currentUser: ''});
+        this.setState({contacts: []});
         this.updateList()
     }
 
-    getPairID(sender, receiver) {
-        let key = '';
-        if (sender === receiver) {
-            key = sender;
-        } else if (sender > receiver) {
-            key = receiver + sender;
-        } else {
-            key = sender + receiver;
-        }
-        return key;
-    }
-
-    async getLastActiveTime(sender, receiver) {
-        let lastchatTime = 0;
-        let key = this.getPairID(sender, receiver);
-        await firebase.database().ref('conversations').once('value', async (snap) => {
-            if (snap.hasChild(key)) {
-                let time = snap.child(key).val().lastActiveTime;
-                if (time) {
-                    lastchatTime = time;
-                    return time;
-                }
-            }
-        });
-        return lastchatTime;
-    }
-
-    async requestContactsPermission() {
-        try {
-            const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.READ_CONTACTS
-            );
-            return granted === PermissionsAndroid.RESULTS.GRANTED;
-        } catch (error) {
-            return Platform.OS === "ios" ? true : false;
-        }
-    }
-
-    async getOrderedLocalContacts() {
-        let db = firebase.database();
-        let localContacts = [];
-        Contacts.getAll((err, contacts) => {
-            if (err) throw err;
-            else {
-                db.ref("registeredUsers").once("value", async registeredUsers => {
-                    const time = await this.getLastActiveTime(sender, sender);
-                    localContacts.push({
-                        key: sender,
-                        name: "You",
-                        lastActiveTime: time
-                    });
-                    for (let i = 0; i < contacts.length; i++) {
-                        if (contacts[i].phoneNumbers.length !== 0) {
-                            let number = contacts[i].phoneNumbers[0].number.replace(
-                                /\D/g,
-                                ""
-                            );
-                            let trimmedNumber = number;
-                            if (number.length == 12) {
-                                trimmedNumber = number.substring(2);
-                            }
-                            number = trimmedNumber;
-                            if (number) {
-                                if (number && registeredUsers.hasChild(number)) {
-                                    const time = await this.getLastActiveTime(sender, number);
-                                    localContacts.push({
-                                        key: number,
-                                        name: contacts[i].givenName,
-                                        lastActiveTime: time
-                                    });
-                                    AsyncStorage.setItem(number, contacts[i].givenName);
-                                }
-                            }
-                        }
-                    }
-                    localContacts.sort(function (contact1, contact2) {
-                        return contact2.lastActiveTime - contact1.lastActiveTime;
-                    });
-                    this.setState({
-                        contacts: localContacts
-                    })
-                    await AsyncStorage.setItem('listofContacts', JSON.stringify(localContacts))
-                });
-                let dbUserProfileRef = db.ref("registeredUserProfileInfo").child(sender);
-                let dbLocalContRef = dbUserProfileRef.child("LocalContacts");
-                dbLocalContRef.once("value", (contactsFromDb) =>{
-                    let localContactsFromDb = contactsFromDb.val();
-                    let myLocalContacts = this.state.contacts;
-                    for(let index = 0; index<this.state.contacts.length; index++){
-                        if(!contactsFromDb.hasChild(myLocalContacts[index].key)){
-                            console.log("It has the child")
-                            dbLocalContRef.child(myLocalContacts[index].key).set(myLocalContacts[index].name);
-                        }
-                    }
-                })
-            }
-        });
-    }
-
-    async updateList(){
-        let response = await AsyncStorage.getItem('listofContacts');
-        if(response === null){
-            this.getOrderedLocalContacts()
-        }
-        else{
-            let listofContacts = await JSON.parse(response)||[];
-            this.setState({
-                contacts:listofContacts
-            });
-            this.getOrderedLocalContacts()
-        }
-    }
-
     listenForVideoCall() {
-        VIDEO_CALL_REF.child(sender).child("flag").set(true);
-        VIDEO_CALL_REF.child(sender).on("value", (snapshot) => {
+        VIDEO_CALL_INFO_REF.child(sender).child("flag").set(true);
+        VIDEO_CALL_INFO_REF.child(sender).on("value", (snapshot) => {
             let videoCallInfo = snapshot.val();
             if (caller !== null && videoCallInfo !== null && caller === videoCallInfo.caller) {
-                console.log("one");
+                return;
             }
             else {
                 caller = videoCallInfo !== null ? videoCallInfo.caller : null;
-                console.log("two")
             }
             if (caller && videoCallInfo != null) {
-                // this.listenOnCaller();
-                console.log("three")
                 this.props.navigation.navigate("AnswerVideoCall", {
                     callee: this.props.navigation.getParam("sender"),
                     caller: caller
-                    // onGoBack: () => this.updateCurrentUser()
                 });
             }
         });
     }
 
-    componentWillMount() {
-        sender = this.props.navigation.getParam("sender")
-        console.log("in componentWillMount:" + sender);
-        let dbLocalContRef = firebase.database().ref("registeredUserProfileInfo");
-        dbLocalContRef.child(sender).child("LocalContacts").child(sender).set("You");
-    }
-
-    async componentDidMount() {
-        const permission = await this.requestContactsPermission();
-        if (!permission) {
-            alert(permission);
-            return;
-        }
-        this.updateList();
-        this.checkPermission();
-        this.createNotificationListeners();
-        this.listenForVideoCall();
-    }
-
     renderName(contact) {
-        let info = {
-            sender: this.props.navigation.getParam("sender"),
-            receiver: contact.item.key
-        };
+        info.sender = sender;
+        info.receiver = contact.item.key;
         return (
             <TouchableOpacity onPress={() => {
-                this.props.navigation.navigate('ChatScreen', { info: info, contactName: contact.item.name, onGoBack: () => this.updateCurrentUser() });
-                this.setState({ currentUser: contact.item.name })
+                this.props.navigation.navigate('ChatScreen', {
+                    info: info,
+                    contactName: contact.item.name,
+                    onGoBack: () => this.updateCurrentUser()
+                });
+                this.setState({currentUser: contact.item.name})
             }} style={styles.contactContainer}>
-                <Profile sender={contact.item.key} />
+                <Profile sender={contact.item.key}/>
                 <Text style={styles.item}> {contact.item.name} </Text>
             </TouchableOpacity>
         );
