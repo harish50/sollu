e24de81cbfdd935bb77f0cd389e6ecdd0aaa3a76
GAnjali <anjali.gaddam98@gmail.com>
2019-04-06 18:46:50 +0530

    refactor LoginScreen code excluding styles

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
-import { View, TextInput, TouchableOpacity, Text, Image, AsyncStorage, StatusBar } from 'react-native';
+import React, {Component} from 'react';
+import {View, TextInput, TouchableOpacity, Text, Image, AsyncStorage, StatusBar} from 'react-native';
 import styles from "../Stylesheet/styleSheet";
 import firebase from '../firebase/firebase';
-import { NavigationActions, StackActions } from 'react-navigation';
+import {NavigationActions, StackActions} from 'react-navigation';
 
 class LoginScreen extends Component {
+
     constructor(props) {
         super(props)
     }
+
     state = {
         phoneNumber: "",
-    }
+    };
+
     componentDidMount() {
-        AsyncStorage.getItem('userId').then((value) => {
-            this.setState({ userId: value, is_fetching_done: true });
+        //Differentiation between users taking phone number as primary key
+        AsyncStorage.getItem('phoneNumber').then((value) => {
+            this.setState({phoneNumber: value, is_fetching_done: true});
             if (value.length > 1) {
                 this.props.navigation.dispatch(
                     StackActions.reset({
                         index: 0,
-                        actions: [NavigationActions.navigate({ routeName: "HomeScreen", params: { sender: this.state.userId } })]
+                        actions: [NavigationActions.navigate({
+                            routeName: "HomeScreen",
+                            params: {sender: this.state.phoneNumber}
+                        })]
                     })
                 );
             }
@@ -29,11 +36,23 @@ class LoginScreen extends Component {
         this.setState({
             phoneNumber: number
         });
-    }
+    };
+
     handlePress = () => {
+        AsyncStorage.setItem('phoneNumber', this.state.phoneNumber).then(
+            this.props.navigation.dispatch(
+                StackActions.reset({
+                    index: 0,
+                    actions: [NavigationActions.navigate({
+                        routeName: "HomeScreen",
+                        params: {sender: this.state.phoneNumber}
+                    })]
+                })
+            )
+        );
+        //Storing into firebase as a registered user
         let db = firebase.database();
         let taskRef = db.ref('registeredUsers');
-        this.props.navigation.navigate("HomeScreen", { sender: this.state.phoneNumber });
         taskRef.once('value', (registeredUsers) => {
             if (!registeredUsers.hasChild(this.state.phoneNumber)) {
                 taskRef.child(this.state.phoneNumber).set('done');
@@ -45,17 +64,9 @@ class LoginScreen extends Component {
                 userInfoRef.child(this.state.phoneNumber).set('done');
             }
         });
-
-        AsyncStorage.setItem('userId', this.state.phoneNumber).then(
-            this.props.navigation.dispatch(
-                StackActions.reset({
-                    index: 0,
-                    actions: [NavigationActions.navigate({ routeName: "HomeScreen", params: { sender: this.state.phoneNumber } })]
-                })
-            )
-        );
+        this.props.navigation.navigate("HomeScreen", {sender: this.state.phoneNumber});
     };
-    static navigationOptions = ({ navigation }) => {
+    static navigationOptions = ({navigation}) => {
         return (
             {
                 headerTitle: "Sollu",
@@ -72,6 +83,7 @@ class LoginScreen extends Component {
             }
         );
     };
+
     render() {
         if (!this.state.is_fetching_done) {
             return (
@@ -85,7 +97,7 @@ class LoginScreen extends Component {
             <View style={styles.mainBox}>
                 <View style={styles.SectionStyle}>
                     <Image source={require('../Icon/callerIcon3.png')}
-                        style={[styles.imageStyle]} />
+                           style={[styles.imageStyle]}/>
                     <TextInput
                         style={styles.TextContainer}
                         placeholder="Enter phone number"
@@ -96,7 +108,8 @@ class LoginScreen extends Component {
                     />
                 </View>
                 <View>
-                    <TouchableOpacity style={[styles.button, { backgroundColor: this.state.phoneNumber ? '#cc504e' : '#f49f8e' }]}
+                    <TouchableOpacity
+                        style={[styles.button, {backgroundColor: this.state.phoneNumber ? '#cc504e' : '#f49f8e'}]}
                         activeOpacity={.5}
                         disabled={!this.state.phoneNumber}
                         onPress={this.handlePress}>
@@ -107,4 +120,5 @@ class LoginScreen extends Component {
         );
     }
 }
+
 export default LoginScreen;
