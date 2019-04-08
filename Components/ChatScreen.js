bd714800cdc6b54184888458634394ecd69674cb
GAnjali <anjali.gaddam98@gmail.com>
2019-04-08 19:25:06 +0530

    refactored chat screen code excluding UI

--- Components/ChatScreen.js
+++ Components/ChatScreen.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Platform, View, Text, TextInput, KeyboardAvoidingView, FlatList, TouchableOpacity, Image, ImageBackground, ScrollView } from 'react-native';
-import { SafeAreaView, Header, HeaderBackButton } from 'react-navigation';
+import {
+    Platform,
+    View,
+    Text,
+    TextInput,
+    KeyboardAvoidingView,
+    FlatList,
+    TouchableOpacity,
+    ImageBackground
+} from 'react-native';
+import {SafeAreaView, Header, HeaderBackButton} from 'react-navigation';
 import styles from "../Stylesheet/styleSheet";
 import firebase from "../firebase/firebase";
 import Profile from "./Profile";
 import DateComponent from './DateComponent';
 import VideoIconComponent from "./VideoIconComponent";
 
+let navigation = null;
+let info;
+let CONVERSATIONS_REF = firebase.database().ref("conversations");
+let REGISTERED_USERS_REF = firebase.database().ref("registeredUsers");
+let REGISTERED_USER_PROFILE_INFO_REF = firebase.database().ref("registeredUserProfileInfo");
 
 export default class ChatScreen extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -18,15 +33,28 @@ export default class ChatScreen extends React.Component {
             colourDifference: false,
             chatRef: null,
         };
-        this.setLastActiveTime = this.setLastActiveTime.bind(this);
-        this.sendMessage = this.sendMessage.bind(this);
-        this.isColorDiffers = this.isColorDiffers.bind(this);
     }
 
-    getConvoPairID() {
+    componentWillReceiveProps(props) {
+        this.isColorDiffers();
+        info = props.navigation.getParam("info");
+        this.state.chatRef.off();
+        this.getChat(info.sender, info.receiver);
+    }
+
+    componentDidMount() {
+        navigation = this.props;
+        info = this.props.navigation.getParam('info');
+        this.isColorDiffers();
+        CONVERSATIONS_REF.once('value', (conversations) => {
+            if (!conversations.hasChild(this.getPairID()))
+                this.setLastActiveTime(0);
+        });
+        this.getChat(info.sender, info.receiver);
+    }
+
+    getPairID = () => {
         let key = '';
-        let { navigation } = this.props;
-        let info = navigation.getParam('info');
         if (info.sender === info.receiver) {
             key = info.sender;
         }
@@ -37,17 +65,17 @@ export default class ChatScreen extends React.Component {
             key = info.sender + info.receiver;
         }
         return key;
-    }
-    setLastActiveTime(time) {
-        let key = this.getConvoPairID();
-        firebase.database().ref('conversations').child(key).set({ 'lastActiveTime': time });
-    }
-    isColorDiffers() {
+    };
+
+    setLastActiveTime = (time) => {
+        let key = this.getPairID();
+        CONVERSATIONS_REF.child(key).set({'lastActiveTime': time});
+    };
+
+    isColorDiffers = () => {
         let colorDifference = false;
-        let { navigation } = this.props;
-        const info = navigation.getParam('info');
-        const senderInfoRef = firebase.database().ref('registeredUserProfileInfo').child(info.sender);
-        const receiverInfoRef = firebase.database().ref('registeredUserProfileInfo').child(info.receiver);
+        const senderInfoRef = REGISTERED_USER_PROFILE_INFO_REF.child(info.sender);
+        const receiverInfoRef = REGISTERED_USER_PROFILE_INFO_REF.child(info.receiver);
         senderInfoRef.once('value', (senderSnap) => {
             let sender = senderSnap.val();
             receiverInfoRef.on('value', (receiverSnap) => {
@@ -63,18 +91,17 @@ export default class ChatScreen extends React.Component {
                     colorDifference = true;
                 }
             })
-        })
+        });
         this.setState({
             colourDifference: colorDifference
         });
+    };
 
-    }
     getChat = (sender, receiver) => {
-        const db = firebase.database();
-        const chatRef = db.ref('registeredUsers').child(sender).child("chat").child(receiver);
+        const chatRef = REGISTERED_USERS_REF.child(sender).child("chat").child(receiver);
         chatRef.on('value', (data) => {
             let chatData = data.val();
-            let Chat = []
+            let Chat = [];
             for (let chatID in chatData) {
                 const message = {
                     _id: chatData[chatID]._id,
@@ -89,25 +116,9 @@ export default class ChatScreen extends React.Component {
                 chatRef: chatRef,
             });
         });
-    }
-    componentWillReceiveProps(props) {
-        this.isColorDiffers();
-        const info = props.navigation.getParam("info");
-        this.state.chatRef.off();
-        this.getChat(info.sender, info.receiver);
-    }
-    componentDidMount() {
-        this.isColorDiffers();
-        const info = this.props.navigation.getParam("info")
-        firebase.database().ref('conversations').once('value', (conversations) => {
-            if (!conversations.hasChild(this.getConvoPairID()))
-                this.setLastActiveTime(0);
-        });
-        this.getChat(info.sender, info.receiver);
-    }
+    };
 
-    static navigationOptions = ({ navigation }) => {
-        let props = navigation
+    static navigationOptions = ({navigation}) => {
         return (
             {
                 headerTitle: navigation.getParam("contactName"),
@@ -118,21 +129,24 @@ export default class ChatScreen extends React.Component {
                     backgroundColor: '#cc504e',
                     height: 60,
                 },
-                headerRight: ([<VideoIconComponent info={navigation.getParam("info")} contactName={navigation.getParam("contactName")} navigation={props} />, <Profile sender={navigation.getParam("info").receiver} />]),
+                headerRight: ([<VideoIconComponent info={navigation.getParam("info")}
+                                                   contactName={navigation.getParam("contactName")}
+                                                   navigation={navigation}/>,
+                    <Profile sender={navigation.getParam("info").receiver}/>]),
                 headerLeft: (
                     <HeaderBackButton tintColor="white"
-                     onPress={() => { navigation.state.params.onGoBack(); navigation.goBack(); }} />)
+                                      onPress={() => {
+                                          navigation.state.params.onGoBack();
+                                          navigation.goBack();
+                                      }}/>)
             }
         );
     };
 
-    sendMessage() {
+    sendMessage = () => {
         if (this.state.typing.trim() === '') {
             return;
         }
-        let { navigation } = this.props;
-        let db = firebase.database();
-        const info = navigation.getParam('info');
         let msg = {
             _id: 2,
             text: this.state.typing.trim(),
@@ -140,28 +154,25 @@ export default class ChatScreen extends React.Component {
         };
         if (info.sender === info.receiver) {
             msg._id = 0;
-            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver).push(msg);
-
+            REGISTERED_USERS_REF.child(info.sender).child("chat").child(info.receiver).push(msg);
         }
         else {
-            db.ref('registeredUsers').child(info.sender).child("chat").child(info.receiver).push(msg);
+            REGISTERED_USERS_REF.child(info.sender).child("chat").child(info.receiver).push(msg);
             msg._id = 1;
-            db.ref('registeredUsers').child(info.receiver).child("chat").child(info.sender).push(msg);
+            REGISTERED_USERS_REF.child(info.receiver).child("chat").child(info.sender).push(msg);
         }
         this.setLastActiveTime(msg.createdAt);
-        this.setState({ typing: '' });
-    }
+        this.setState({typing: ''});
+    };
 
-    renderItem({ item }) {
+    renderItem({item}) {
         if (item.header) {
             return (
-                <DateComponent item={item} />
+                <DateComponent item={item}/>
             );
         }
         let messageboxstyle;
         let messagetextstyle;
-        let { navigation } = this.props;
-        const info = navigation.getParam('info');
         let phoneNo = info.sender;
         if (item._id === 0) {
             messageboxstyle = styles.selfMessageContainer;
@@ -191,8 +202,8 @@ export default class ChatScreen extends React.Component {
             return (
                 <View style={messageboxstyle}>
                     <View>
-                        <ImageBackground source={Icon} style={styles.chatIcon} imageStyle={{ borderRadius: 100 }}>
-                            <Profile sender={phoneNo} />
+                        <ImageBackground source={Icon} style={styles.chatIcon} imageStyle={{borderRadius: 100}}>
+                            <Profile sender={phoneNo}/>
                         </ImageBackground>
                     </View>
                     <View style={messagetextstyle}>
@@ -205,7 +216,7 @@ export default class ChatScreen extends React.Component {
         else {
             return (
                 <View style={messageboxstyle}>
-                    <Profile sender={phoneNo} />
+                    <Profile sender={phoneNo}/>
                     <View style={messagetextstyle}>
                         <Text style={styles.messageStyle}>{item.text}</Text>
                         <Text style={styles.timeStyle}>{time}</Text>
@@ -228,7 +239,7 @@ export default class ChatScreen extends React.Component {
             header: true
         });
         for (let counter = 0; counter < messagesLength; counter++) {
-            day = monthNames[messages[counter].createdAt.getMonth()] + " " + messages[counter].createdAt.getDate() + " " + messages[counter].createdAt.getFullYear();
+            let day = monthNames[messages[counter].createdAt.getMonth()] + " " + messages[counter].createdAt.getDate() + " " + messages[counter].createdAt.getFullYear();
             if (day !== lastDate) {
                 daywiseMessages.push({
                     text: day,
@@ -237,9 +248,11 @@ export default class ChatScreen extends React.Component {
                 lastDate = day;
             }
             daywiseMessages.push(messages[counter]);
-        };
+        }
+        ;
         return daywiseMessages;
     }
+
     render() {
         const keyboardVerticalOffset = Platform.OS === 'ios' ? Header.HEIGHT + 35 : 0;
         const padding = Platform.OS === 'ios' ? "padding" : '';
@@ -253,7 +266,9 @@ export default class ChatScreen extends React.Component {
                             renderItem={(item) => this.renderItem(item)}
                             keyExtractor={(item, index) => index.toString()}
                             ref={ref => this.flatList = ref}
-                            onContentSizeChange={() => { this.flatList.scrollToEnd({ animated: false }) }}
+                            onContentSizeChange={() => {
+                                this.flatList.scrollToEnd({animated: false})
+                            }}
                         />
                     </View>
 
@@ -264,7 +279,7 @@ export default class ChatScreen extends React.Component {
                         <View style={styles.footer}>
                             <TextInput
                                 value={this.state.typing}
-                                onChangeText={text => this.setState({ typing: text })}
+                                onChangeText={text => this.setState({typing: text})}
                                 style={styles.input}
                                 underlineColorAndroid="transparent"
                                 placeholder="Type something nice"
