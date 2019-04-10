8d8348461dbec654c62e88da64a77d43f8b67e57
ganjali <gaddam.anjali@oracle.com>
2019-04-10 22:52:08 +0530

    tests for checking login View

--- src/Screens/Login/__tests__/LoginViewTest.js
+++ src/Screens/Login/__tests__/LoginViewTest.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import LoginView from '../LoginView';
-import renderer from 'react-test-renderer';
+import {mount, shallow} from 'enzyme';
+import LoginView from "../LoginView";
+import {Image, Text, TextInput, TouchableOpacity, View, Button} from 'react-native';
 
-test('renders correctly', () => {
-    const tree = renderer.create(<LoginView />).toJSON();
-    expect(tree).toMatchSnapshot();
+let component;
+beforeEach(() => {
+     component = shallow(<LoginView />);
+});
+
+describe('LoginComponent rendering', () =>{
+
+  it('should have LoginComponent',()=>{
+      expect(component).toMatchSnapshot();
+  });
+
+    it("should have image", ()=>{
+        expect(component.find(Image)).toHaveLength(1);
+    });
+
+    it("should not have button", ()=>{
+        expect(component.find(Button)).toHaveLength(0);
+    });
+
+    it("should have one view", ()=>{
+       expect(component.find(View)).toHaveLength(3);
+    });
+
+    it("should have one touchablreOpacity",() =>{
+        expect(component.find(TouchableOpacity)).toHaveLength(1);
+    })
 });
