bed3735e4bcf80fd79dc5bb3ec60cef194059599
ganjali <gaddam.anjali@oracle.com>
2019-04-12 08:50:46 +0530

    Seperating render part as presentational component from Home Screen

--- src/Screens/Home/HomeContainer.js
+++ src/Screens/Home/HomeContainer.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react';
 import HomeView from './HomeView'
 
 export default class HomeContainer extends Component {
+
+    state={
+        contacts: []
+    };
+
+
     render() {
         return (
-            <HomeView />
+            <HomeView contacts={this.state.contacts}/>
         )
     }
 }
