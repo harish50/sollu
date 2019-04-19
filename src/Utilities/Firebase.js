148b4e8a4873e3249561baecf74caeee7b3fced4
ganjali <gaddam.anjali@oracle.com>
2019-04-19 17:28:02 +0530

    moved ChatPage functionality of firebase into ChatServie from Utilities/firebase.js

--- src/Utilities/Firebase.js
+++ src/Utilities/Firebase.js
@@ -1,6 +1,5 @@
 import firebase from '../../firebase/firebase';
 import {STRINGS} from "./StringsStore";
-let REGISTERED_USERS_REF = firebase.database().ref("registeredUsers");
 
 const usersRef = firebase.database().ref('Users');
 export const registerUser = async (phoneNumber) => {
@@ -16,37 +15,4 @@ export const registerUser = async (phoneNumber) => {
         return error;
     });
     return true;
-};
-
-export const sendMessage = (sender, receiver, message) => {
-    REGISTERED_USERS_REF.child(sender).child("chat").child(receiver).push(message);
-};
-
-
-export const getChat = (sender, receiver) =>{
-    return new Promise(function (resolve, reject) {
-        try {
-            const chatRef = REGISTERED_USERS_REF.child(sender).child("chat").child(receiver);
-            chatRef.on('value', function (data) {
-                if (typeof data.val() === 'undefined') {
-                    resolve(null);
-                } else {
-                    console.log("in getchat:",data.val());
-                    const chatData = data.val();
-                    let chatMessages = [];
-                    for (let chatID in chatData) {
-                        const message = {
-                            _id: chatData[chatID]._id,
-                            text: chatData[chatID].text,
-                            createdAt: new Date(chatData[chatID].createdAt),
-                        };
-                        chatMessages.push(message);
-                    }
-                    resolve(chatMessages);
-                }
-            })
-        } catch (e) {
-            reject(e)
-        }
-    });
 };
