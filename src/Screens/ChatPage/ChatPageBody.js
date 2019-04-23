e0095fda00f38c641faf13e0a35d946c5e9557a7
HemaSree1519 <khemasree96@gmail.com>
2019-04-23 08:51:13 +0530

    Separate the chat page body into a component

---
+++ src/Screens/ChatPage/ChatPageBody.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+import {FlatList, Text, View} from "react-native";
+import styles from "./ChatStyles";
+
+export default class ChatPageBody extends Component {
+    render() {
+        console.log("Chat Content called'")
+        if (this.props.messages.length > 0) {
+            return (
+                <View style={styles.container}>
+                    <FlatList
+                        data={this.props.messages}
+                        extraData={this.props.messages}
+                        renderItem={(item) => this.props.renderMessage(item.item)}
+                        keyExtractor={(item, index) => index.toString()}
+                        ref={ref => this.flatList = ref}
+                        onContentSizeChange={() => {
+                            this.flatList.scrollToEnd({animated: false})
+                        }}
+                    />
+                </View>)
+        }
+        else {
+            return (
+                <View style={styles.noChatContainer}>
+                    <Text style={styles.noChatText}>No conversation</Text>
+                </View>)
+        }
+    }
+}
