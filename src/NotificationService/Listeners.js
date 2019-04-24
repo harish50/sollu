b33c653751471504370eca7758844c77a8f022cc
HemaSree1519 <khemasree96@gmail.com>
2019-04-24 22:21:38 +0530

    Restructure message notification listeners for background

--- src/NotificationService/Listeners.js
+++ src/NotificationService/Listeners.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {checkPermissionToNotification} from "./Permission";
 import {foregroundListener} from "./ForegroundListener";
 import {onNotifOpenHandler} from "./onNotifOpenHandler";
+import {backgroundListener} from "./BackgroundListener";
 
 export const createNotificationListeners = async (navigation) => {
     console.log("Creating listeners")
     checkPermissionToNotification();
     foregroundListener();
+    backgroundListener(navigation);
     onNotifOpenHandler(navigation);
 };
 
