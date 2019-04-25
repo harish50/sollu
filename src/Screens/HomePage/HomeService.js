226b51287ad2698f7484e31e52cfd75e3c080def
HemaSree1519 <khemasree96@gmail.com>
2019-04-25 23:54:36 +0530

    Store individual's local contacts in firebse to display sender name in the notification

--- src/Screens/HomePage/HomeService.js
+++ src/Screens/HomePage/HomeService.js
@@ -1,7 +1,9 @@
 import firebase from "../../../firebase/firebase";
 import _ from 'lodash'
+import {AsyncStorage} from 'react-native'
 
 const dbRef = firebase.database();
+let REGISTERED_USER_PROFILE_INFO_REF = dbRef.ref("registeredUserProfileInfo");
 export const getData = (path) => {
     return new Promise(function (resolve, reject) {
         try {
@@ -16,5 +18,18 @@ export const getData = (path) => {
         } catch (e) {
             reject(e)
         }
+    })
+};
+
+export const saveLocalContactNamesInDB = async (localContacts) => {
+    let phoneNumber = await AsyncStorage.getItem('PhoneNumber');
+    REGISTERED_USER_PROFILE_INFO_REF.child(phoneNumber).child("LocalContacts").child(phoneNumber).set("You");
+    let localContactsRef = REGISTERED_USER_PROFILE_INFO_REF.child(phoneNumber).child("LocalContacts");
+    localContactsRef.once("value", (contactsFromDb) => {
+        _.each(localContacts, function (contact) {
+            if (!contactsFromDb.hasChild(contact.key)) {
+                localContactsRef.child(contact.key).set(contact.name);
+            }
+        })
     })
 };
