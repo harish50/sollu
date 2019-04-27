5b080b3870e89702c5723a6d05436cdb7dac7928
Harika-Raj <r121662@rguktrkv.ac.in>
2019-04-27 05:53:00 +0530

    modify Test cases for loginView

--- src/Screens/LoginPage/__tests__/LoginViewTest.js
+++ src/Screens/LoginPage/__tests__/LoginViewTest.js
@@ -1,45 +1,28 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import LoginView from "../LoginView";
-import {Image,TouchableOpacity, View, Button} from 'react-native';
+import {TouchableOpacity,TextInput} from 'react-native';
 import renderer from "react-test-renderer"
 
-let component;
-beforeEach(() => {
-    component = shallow(<LoginView />);
-});
 
 describe('LoginComponent rendering', () =>{
 
-    it('should have LoginComponent',()=>{
+    test('should have LoginComponent',()=>{
         expect(component).toMatchSnapshot();
     });
 
-    it("should have image", ()=>{
-        expect(component.find(Image)).toHaveLength(1);
-    });
-
-    it("should not have button", ()=>{
-        expect(component.find(Button)).toHaveLength(0);
-    });
-
-    it("should have one view", ()=>{
-        expect(component.find(View)).toHaveLength(3);
-    });
-
-    it("should have one touchablreOpacity",() =>{
-        expect(component.find(TouchableOpacity)).toHaveLength(1);
-    })
-    it("should state if phone number is entered", () => {
+    test("should check if phone number is entered", () => {
         const instanceOf = renderer.create(<LoginView/>).getInstance();
         instanceOf.setNumber('9505517958');
         expect(instanceOf.state.phoneNumber).toEqual('9505517958')
-    })
-    test("onPress function is called only once",() => {
+    });
+
+    test("should call on login with given phone number",() => {
         const fn = jest.fn();
-        const component = renderer.create(<LoginView onPress={fn} />);
-        const instance = component.getInstance();
-        instance.props.onPress();
-        expect(fn.mock.calls.length).toBe(1);
-    })
+        const component = shallow(<LoginView onLogin={fn} />);
+        component.find(TextInput).at(0).simulate('changeText','9505517958')
+        component.update()
+        component.find(TouchableOpacity).at(0).simulate('press')
+        expect(fn).toBeCalledWith('9505517958')
+    });
 });
